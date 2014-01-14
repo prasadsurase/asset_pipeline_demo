8f6a507c00043a046bb50024abf60e702ed606ee
Prasad Surase <prasad@Matrix.(none)>
2014-01-14 21:59:33 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssetPipelineDemo::Application.config.secret_key_base = 'df7e327b4cbc1e15d552f0eb7b1eb24b1dc389c05b76d284825d2cc0dae86263ababf09c94f2bf707a3e22e7920154bc21a2b6c287dd715be6d971f377ef036a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AssetPipelineDemo::Application.config.session_store :cookie_store, key: '_asset_pipeline_demo_session'
